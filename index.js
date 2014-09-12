9757a865c16502e2f5df5206fa04069563dc8678
Feross Aboukhadijeh <feross@feross.org>
2014-09-12 01:37:08 +0100

    fix readFloat bug

--- index.js
+++ index.js
@@ -800,7 +800,7 @@ function checkIEEE754 (buf, value, offset, ext, max, min) {
 
 function writeFloat (buf, value, offset, littleEndian, noAssert) {
   if (!noAssert)
-    checkIEEE754(this, value, offset, 4, 3.4028234663852886e+38, -3.4028234663852886e+38)
+    checkIEEE754(buf, value, offset, 4, 3.4028234663852886e+38, -3.4028234663852886e+38)
   ieee754.write(buf, value, offset, littleEndian, 23, 4)
   return offset + 4
 }
