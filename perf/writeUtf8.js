15330d03aa981ad9e2e0c6a2e7ff9a1ef4f5eef4
Jesse Tane <jesse.tane@gmail.com>
2014-12-19 15:40:25 -0500

    add benchmark for encoding to utf8

---
+++ perf/writeUtf8.js
@@ -0,0 +1,26 @@
+var BrowserBuffer = require('../').Buffer // (this module)
+var util = require('./util')
+var suite = util.suite()
+
+var LENGTH = 9
+var singleByte = 'abcdefghi'
+var multiByte =  '\u0610' + '\u6100' + '\uD944\uDC00'
+
+var browserBuffer = new BrowserBuffer(LENGTH)
+var nodeBuffer = new Buffer(LENGTH)
+
+suite
+  .add('BrowserBuffer#singleByte', function () {
+    browserBuffer.write(singleByte)
+  })
+  .add('BrowserBuffer#multiByte', function () {
+    browserBuffer.write(multiByte)
+  })
+
+if (!process.browser) suite
+  .add('NodeBuffer#singleByte', function () {
+    nodeBuffer.write(singleByte)
+  })
+  .add('NodeBuffer#multiByte', function () {
+    nodeBuffer.write(multiByte)
+  })
