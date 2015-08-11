4af68f5b34e5e78271c7e24fcd1ecbca3690b69d
Jesse Tane <jesse.tane@gmail.com>
2015-08-10 23:36:15 -0400

    unicode top should be 0x10FFFF

--- index.js
+++ index.js
@@ -1458,7 +1458,7 @@ function utf8ToBytes (string, units) {
         codePoint >> 0x6 & 0x3F | 0x80,
         codePoint & 0x3F | 0x80
       )
-    } else if (codePoint < 0x200000) {
+    } else if (codePoint < 0x110000) {
       if ((units -= 4) < 0) break
       bytes.push(
         codePoint >> 0x12 | 0xF0,
