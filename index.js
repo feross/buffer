d4622d64f3aec3ce8a342f83b33c1ae7ae7ca962
James Halliday <mail@substack.net>
2016-01-28 13:56:12 -0800

    disable value check on floats to match node behavior

--- index.js
+++ index.js
@@ -1193,7 +1193,6 @@ Buffer.prototype.writeInt32BE = function writeInt32BE (value, offset, noAssert) 
 }
 
 function checkIEEE754 (buf, value, offset, ext, max, min) {
-  if (value > max || value < min) throw new RangeError('value is out of bounds')
   if (offset + ext > buf.length) throw new RangeError('index out of range')
   if (offset < 0) throw new RangeError('index out of range')
 }
