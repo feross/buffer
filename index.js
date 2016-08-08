50b05261fd4bb08b643ef0f31b9d8ad8390a9258
Feross Aboukhadijeh <feross@feross.org>
2016-08-08 01:16:12 -0700

    Add explanatory comment
    In response to @dcposch's feedback

--- index.js
+++ index.js
@@ -231,6 +231,9 @@ function fromString (that, string, encoding) {
   var actual = that.write(string, encoding)
 
   if (actual !== length) {
+    // Writing a hex string, for example, that contains invalid characters will
+    // cause everything after the first invalid character to be ignored. (e.g.
+    // 'abxxcd' will be treated as 'ab')
     that = that.slice(0, actual)
   }
 
