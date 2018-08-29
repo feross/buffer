b651e3a9f2b8394cdb241ae2f6b2d37dc335588d
Feross Aboukhadijeh <feross@feross.org>
2018-08-28 19:41:26 -0700

    standard

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function typedArraySupport () {
   // Can typed array instances can be augmented?
   try {
     var arr = new Uint8Array(1)
-    arr.__proto__ = {__proto__: Uint8Array.prototype, foo: function () { return 42 }}
+    arr.__proto__ = { __proto__: Uint8Array.prototype, foo: function () { return 42 } }
     return arr.foo() === 42
   } catch (e) {
     return false
